c9d0d4ca6d717a69590f4ccd667d5430c4f0bef9
Corey Norris <coreypnorris@gmail.com>
2015-03-17 20:48:29 -0700

    set up and document how to set up basic MongoDB database

--- config/express.js
+++ config/express.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var logger = require('morgan');
+
 module.exports = function() {
   var app = express();
   app.set('views', './app/views');
   app.set('view engine', 'ejs');
-  require('../app/routes/index.server.routes.js')(app);
   app.use(express.static('./public'));
+
+  require('../app/routes/index.server.routes.js')(app);
 
   app.use(logger('default'));
 
