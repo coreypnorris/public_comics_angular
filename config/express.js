58a73381ed34ec716bd0795e2b88577bee4e0333
Corey Norris <coreypnorris@gmail.com>
2015-03-28 20:27:09 -0700

    refactor documentation

--- config/express.js
+++ config/express.js
@@ -1,28 +1,9 @@
 var express = require('express');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-
 module.exports = function() {
   var app = express();
   app.set('views', './app/views');
   app.set('view engine', 'ejs');
-  app.use(express.static('./public'));
-
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-
-  app.use(bodyParser.json());
-
   require('../app/routes/index.server.routes.js')(app);
-  require('../app/routes/users.server.routes.js')(app);
-
-  app.use(logger('default'));
-
-  // Handle 404
-  app.use(function(req, res, next) {
-      res.status(404).sendfile('./error/404.html');
-  });
-
+  app.use(express.static('./public'));
   return app;
 };
--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-var config = require('./config/config'),
-  mongoose = require('./config/mongoose'),
-  express = require('./config/express'),
-
-  db = mongoose(),
-  app = express();
-
-app.listen(config.port);
-
+var port = 3000;
+var express = require('./config/express');
+var app = express();
+app.listen(port);
 module.exports = app;
-console.log(process.env.NODE_ENV  + ' server running at http://localhost:' + config.port);
+console.log('Server running at http://localhost:' + port);
