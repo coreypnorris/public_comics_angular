265ef776e2eb89398d1e9231b68d3e9ad2cf170d
Corey Norris <coreypnorris@gmail.com>
2015-04-19 14:58:58 -0700

    start from scratch

---
+++ PublicComicsAngular/app/app.js
@@ -0,0 +1,42 @@
+﻿var app = angular.module("app", ['ui.router']);
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            controller: 'HomeStateCtrl',
+            templateUrl: 'templates/home.html'
+        })
+        .state('upload', {
+            url: '/upload',
+            templateUrl: 'templates/upload.html'
+        })
+
+    $urlRouterProvider.otherwise('/home');
+});
+
+// Home State Scripts
+var HomeStateCtrl = function ($scope) {
+    $scope.comics = [    
+        {
+            "id": 1,
+            "title_name": "Captain Marvel Adventures",
+            "title_id": 1,
+            "user_id": 1,
+            "created_at": 1288323623006,
+            "uploaded_at": 1429480382,
+            "pages": [
+                "images/0.jpg",
+                "images/1.jpg",
+                "images/2.jpg",
+                "images/3.jpg",
+                "images/4.jpg",
+                "images/5.jpg",
+                "images/6.jpg",
+                "images/7.jpg",
+                "images/8.jpg",
+                "images/9.jpg",
+                "images/10.jpg"
+            ]
+        }
+    ];
+};
---
+++ PublicComicsAngular/notes.md
@@ -0,0 +1,17 @@
+﻿todo
+---
+- Build login link
+- Build user authentication form
+- Build user authentication backend
+
+
+-AngularApp
+--AngularApp.js
+
+Stories
+---
+-- As a user, I need to retrieve my password
+- I go to the login page
+- I click the 'Forgot Password' link
+- I answer the password question correctly
+- I get an email with my password in it
